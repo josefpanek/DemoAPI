6a890b53a4b47d625101609ee0d926682edf005f
Pepa Pánek <josef.panek@gmail.com>
2022-05-09 20:01:51 +0200

    Add project files.

---
+++ DemoAPI/Controllers/DemoController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoAPI.Controllers
+{
+    [Route("api/demo")]
+    public class DemoController : ControllerBase
+    {
+        private static List<TemperatureDto> temperatureList = new List<TemperatureDto>
+        { new TemperatureDto
+        {
+            Timestamp = DateTime.Now,
+            Value = 5.6
+        },new TemperatureDto
+        {
+            Timestamp = DateTime.Now.AddDays(1),
+            Value = 15.7
+        } };
+
+
+
+        [HttpGet("get1")]
+        public List<TemperatureDto> GetTemperatures()
+        {
+            return temperatureList;
+        }
+
+        [HttpGet("get2/{id:int}")]
+        public List<TemperatureDto> GetTemperatures2(int id)
+        {
+            return temperatureList;
+        }
+    }
+    public class TemperatureDto
+    {
+        public DateTime Timestamp { get; set; }
+        public double Value { get; set; }
+    }
+}
